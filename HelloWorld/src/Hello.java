1c447be0f5ea445065302f5047a2969d794ee5e9
Akshal27 <akshaljasmine2711@gmail.com>
2022-05-19 17:11:39 +0530

    Start HelloWorld

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,27 @@
+public class Hello {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//prints hello, Akshal!
+		System.out.println("Hello, Akshal !");
+		
+		//defining int variables and operation preference
+		
+		int myFirstNumber = (5 + 7)+(2*10);
+		
+		int mySecondNumber = 12;
+		
+		int myThirdNumber = 6;
+		
+		int myTotal = myFirstNumber * 2;
+		
+		int myLastOne = 1000 - myTotal ;
+				
+//		System.out.println(myFirstNumber);
+//	    you can either type sysout or sout in eclipse (shortcut)
+				
+		System.out.println(myTotal);
+		System.out.println(myLastOne);
+	}
+}
