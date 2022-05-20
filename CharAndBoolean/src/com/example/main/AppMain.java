c79443b51bdce40e98c71e923a09516613d7fcd3
Akshal27 <akshaljasmine2711@gmail.com>
2022-05-20 12:30:07 +0530

    Define Char and Boolean

---
+++ CharAndBoolean/src/com/example/main/AppMain.java
@@ -0,0 +1,29 @@
+package com.example.main;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		
+		//using char you can store a single character
+		
+		char myChar = 'D';
+		
+		char myUnicode = '\u0044';
+		
+		System.out.println("My char : "+myChar);
+		System.out.println("My unicode: "+myUnicode);
+		
+		char myCopyrightChar = '\u00A9';
+		
+		System.out.println(myCopyrightChar);
+		
+		boolean myTrueBooleanValue = true;
+		boolean myFalseBooleanValue = false;
+		
+		boolean isCustomerOverTwentyOne = true;
+		
+		System.out.println(isCustomerOverTwentyOne);
+
+	}
+
+}
